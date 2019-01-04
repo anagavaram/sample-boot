52bcd6b3808ce36612fa4a618a407d78e7a545f4
Anil Nagavaram <anagavaram@splunk.com>
2019-01-03 16:21:26 -0800

    update checkout of branch

--- .jenkins/build.groovy
+++ .jenkins/build.groovy
@@ -2,12 +2,12 @@ node {
   echo 'The pipeline started'
   
   def branchVersion = ""
-
   stage ('Checkout') {
     // checkout repository
     checkout scm
 
     // checkout input branch 
     sh "git checkout ${caller.env.BRANCH_NAME}"
+  }
 
   }
