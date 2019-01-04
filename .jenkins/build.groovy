1936d30870b6d6317fdef2826e55e16873ef3a5a
Anil Nagavaram <anagavaram@splunk.com>
2019-01-03 20:38:39 -0800

    updates

--- .jenkins/build.groovy
+++ .jenkins/build.groovy
@@ -7,29 +7,16 @@ node {
     checkout scm
 
     // checkout input branch 
-    sh "git checkout ${caller.env.BRANCH_NAME}"
+    git url: 'https://github.com/anagavaram/sample-boot', branch: '${caller.env.BRANCH_NAME}'
+ //"git checkout ${caller.env.BRANCH_NAME}"
   }
 
-  stage ('Determine Branch Version') {
-    // add maven to path
-    env.PATH = "${tool 'M3'}/bin:${env.PATH}"
-
-    // determine version in pom.xml
-    def pomVersion = sh(script: 'mvn -q -Dexec.executable=\'echo\' -Dexec.args=\'${project.version}\' --non-recursive exec:exec', returnStdout: true).trim()
-
-    // compute proper branch SNAPSHOT version
-    pomVersion = pomVersion.replaceAll(/-SNAPSHOT/, "") 
-    branchVersion = env.BRANCH_NAME
-    branchVersion = branchVersion.replaceAll(/origin\//, "") 
-    branchVersion = branchVersion.replaceAll(/\W/, "-")
-    branchVersion = "${pomVersion}-${branchVersion}-SNAPSHOT"
-
-    // set branch SNAPSHOT version in pom.xml
-    sh "mvn versions:set -DnewVersion=${branchVersion}"
-  }
 
   stage ('Java Build') {
     // build .war package
-    sh 'mvn clean package -U'
+    //sh 'mvn clean package -U'
+
+    String mvnHome = tool("M3");
+    "${mvnHome}/bin/mvn -B verify"
   }
 }
